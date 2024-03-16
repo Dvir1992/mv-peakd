179c919a6bf67ba2db9d034b7127eb9426cebb9d
Dvir Hazut <53955899+Dvir1992@users.noreply.github.com>
2024-03-16 19:20:10 +0200

    Create pd_mavg_aex_includes.v

---
+++ pd_mavg_aex_includes.v
@@ -0,0 +1,6 @@
+`define DATAWIDTH 12 // data path width in bits
+`define ROW_WIDTH 9 // ROW NUM of bits needed to define regs (for 480 rows we need 9 bits)
+`define PIXEL_WIDTH 10 // PIXELS NUM of bits needed to define regs (for 1024 pixels we need 10 bits)
+`define PIXEL_OVER_NUM 10 // number of pixels in aex above treshold (register in reg blk)
+`define SERIALOUT_BITS_NUM 70 // number of bits transmite on serial (2+6)x10 bits  
+`define INDEX_WIDTH 11 // index counter
